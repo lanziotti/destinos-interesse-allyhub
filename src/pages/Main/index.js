9a1c945090aa644d0d60e3eeaa6489fb08b4e188
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-10-26 18:50:15 -0300

    Organização inicial do projeto

---
+++ src/pages/Main/index.js
@@ -0,0 +1,15 @@
+import './styles.css';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+function Main() {
+  return (
+    <div>
+      <Header />
+      <h1>Main</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default Main;
