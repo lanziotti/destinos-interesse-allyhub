3d0334b43ca7f5e3a88e60fd040988a0b96839bb
Rodrigo Lanziotti <rodrigolanziotti@yahoo.com.br>
2022-10-27 12:50:02 -0300

    Acabando criação e estilização da Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -7,7 +7,57 @@ function Main() {
     <div className='container'>
       <Header />
       <main>
-        <h1>Main</h1>
+        <h1>Que tal fazer aquela sonhada viagem?</h1>
+        <h2>Pesquise aqui seu possível destino!</h2>
+        <div className='container-forms'>
+          <form>
+            <div className='container-squares'>
+              <div className='content-user'>
+                <h3>Dados Pessoais</h3>
+                <div className='content-user-data'>
+                  <label htmlFor='name'>Nome</label>
+                  <input
+                    type='text'
+                    name='name'
+                    required
+                  />
+                  <label htmlFor='email'>E-mail</label>
+                  <input
+                    type='email'
+                    name='email'
+                    required
+                  />
+                  <label htmlFor='telefone'>Telefone</label>
+                  <input
+                    type='tel'
+                    name='telefone'
+                    required
+                  />
+                  <label htmlFor='cpf'>CPF</label>
+                  <input
+                    type='text'
+                    name='cpf'
+                    required
+                  />
+                </div>
+              </div>
+              <div className='content-destinations'>
+                <h3>Destinos de Interesse</h3>
+                <div className='content-current-destination'>
+                  <label>País</label>
+                  <select required>
+                    <option>Selecione</option>
+                  </select>
+                  <label>Cidade</label>
+                  <select required>
+                    <option>Selecione</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <button>ENVIAR</button>
+          </form>
+        </div>
       </main>
       <Footer />
     </div>
